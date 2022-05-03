18b9e6d01dca4f35cb07e5f0d7d00146b9535c46
luka kolonic <matea.zuvic@gmail.com>
2022-05-03 17:03:23 +0200

    Implementiran prvi dio Web App

---
+++ eSouvenirWebApp/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eSouvenirWebApp.Models
+{
+    public class User
+    {
+        public int IDUser { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public byte[] Pass { get; set; }
+
+        public bool IsAdmin { get; set; }
+
+        public ICollection<QRCode> QRCodes { get; set; }
+
+        public User(string username, string email, byte[] pass, bool isAdmin)
+        {
+            Username = username;
+            Email = email;
+            Pass = pass;
+            IsAdmin = isAdmin;
+        }
+    }
+}
